cc9f51a97be11a49e92c17610d95a46154dbb207
rmacedod <rogerdrumond@hotmail.com>
2023-03-30 17:27:40 -0300

    QUINTO Commit

--- src/test/java/leilao/LeiloesPage.java
+++ src/test/java/leilao/LeiloesPage.java
@@ -1,10 +1,13 @@
 package leilao;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LeiloesPage {
 
     private static final String URL_CADASTRO_LEILAO = "http://localhost:8080/leiloes/new";
+    private static final String URL_LEILOES = "http://localhost:8080/leiloes";
     private WebDriver browser;
 
     public LeiloesPage(WebDriver browser) {
@@ -20,5 +23,20 @@ public class LeiloesPage {
         this.browser.navigate().to(URL_CADASTRO_LEILAO);
         return new CadastroLeilaoPage(browser);
 
+    }
+
+    public boolean isLeilaoCadastrado(String nome, String valor, String data) {
+        WebElement linhaDaTabela = this.browser.findElement(By.cssSelector("#tabela-leiloes tbody tr:last-child"));
+        WebElement colunaNome = linhaDaTabela.findElement(By.cssSelector("td:nth-child(1)"));
+        WebElement colunaDataAbertura = linhaDaTabela.findElement(By.cssSelector("td:nth-child(2)"));
+        WebElement colunaValorInicial = linhaDaTabela.findElement(By.cssSelector("td:nth-child(3)"));
+
+        return colunaNome.getText().equals(nome)
+                && colunaDataAbertura.getText().equals(data)
+                && colunaValorInicial.getText().equals(valor);
+    }
+
+    public boolean isPaginaAtual() {
+        return browser.getCurrentUrl().equals(URL_LEILOES);
     }
 }
--- src/test/java/leilao/LeiloesTest.java
+++ src/test/java/leilao/LeiloesTest.java
@@ -1,13 +1,29 @@
 package leilao;
 
 import Login.LoginPage;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LeiloesTest {
 
     private LeiloesPage paginaDeLeiloes;
+    private CadastroLeilaoPage paginaDeCadastro;
+
+    @BeforeEach
+    public void beforeEach(){
+        LoginPage paginaDeLogin = new LoginPage();
+        paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
+        this.paginaDeLeiloes = paginaDeLogin.efetuaLogin();
+        this.paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+    }
 
     @AfterEach
     public void afterEach(){
@@ -15,10 +31,22 @@ public class LeiloesTest {
     }
     @Test
     public void deveriaCadastrarLeilao() {
-        LoginPage paginaDeLogin = new LoginPage();
-        paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
-        this.paginaDeLeiloes = paginaDeLogin.efetuaLogin();
-        CadastroLeilaoPage paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+        String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String nome = "Leilao do dia" + hoje;
+        String valor = "500.00";
+
+
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome, valor, hoje);
+        Assertions.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome, valor, hoje));
     }
+
+    @Test
+    public void deveriaValidarCadastroDeLeilao() {
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao("","","");
+        Assert.assertFalse(this.paginaDeCadastro.isPaginaAtual());
+        Assert.assertTrue(this.paginaDeLeiloes.isPaginaAtual());
+        Assert.assertTrue(this.paginaDeCadastro.isPaginaDeValidacaoVisiveis());
+    }
+
 
 }
